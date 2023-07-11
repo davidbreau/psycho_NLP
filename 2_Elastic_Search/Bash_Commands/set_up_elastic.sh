350a35cf1e8195f74bfabea4ebc08d390aed3f32
davidbreau <davidbreau@live.fr>
2023-07-11 12:10:45 +0200

    Fix(elastic search mapping): :bug:fix sur les variables d'environement

--- 2_Elastic_Search/Bash_Commands/set_up_elastic.sh
+++ 2_Elastic_Search/Bash_Commands/set_up_elastic.sh
@@ -1,11 +1,8 @@
-# Chargement des variables d'environnement à partir du fichier .env
-set -a
-[ -f .env ] && . .env
-set +a
+# # Chargement des variables d'environnement à partir du fichier .env
+source "$(dirname "$0")/.env" #<--- ELASTIC_DATA_DIR = chemin complet vers miroir elastic search
 
 docker run -d --name elastic \
     -v "$ELASTIC_DATA_DIR" \
-    --net elastic \
     -p 127.0.0.1:9200:9200 -p 127.0.0.1:9300:9300 \
     -e "discovery.type=single-node" \
     docker.elastic.co/elasticsearch/elasticsearch:7.17.10
